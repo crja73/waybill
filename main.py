ebe42c69474715715775759f5ca0b604ef0e2087
crja73 <57744842+crja73@users.noreply.github.com>
2021-01-24 13:45:52 +0300

    5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 al = {'Алтуфьево - Можайка': 36, 'Алтуфьево - Румянцево': 46, 'Алтуфьево - Подольск': 74, 'Алтуфьево - МейлРУ': 25, 'Можайка - Румянцево': 15, 'Можайка - Подольск': 45, 'Можайка - МейлРУ': 21, 'Румянцево - Подольск': 43, 'Румянцево - МейлРУ': 27, 'Румянцево - Можайка': 15, 'МейлРУ - Румянцево': 27, 'МейлРУ - Можайка': 21, 'МейлРУ - Подольск': 50, 'Подольск - МейлРУ': 50, 'Подольск - Румянцево': 43, 'Подольск - Можайка': 45}
 
 cities2 = {'Алтуфьево - Можайка': 36, 'Алтуфьево - Румянцево': 46, 'Алтуфьево - Подольск': 74, 'Алтуфьево - МейлРУ': 25}
@@ -7,7 +8,8 @@ cities3 = {'Можайка - Румянцево': 15, 'Можайка - Подо
 cities4 = {'Румянцево - Подольск': 43, 'Румянцево - МейлРУ': 27, 'Румянцево - Можайка': 15}
 cities5 = {'МейлРУ - Румянцево': 27, 'МейлРУ - Можайка': 21, 'МейлРУ - Подольск': 50}
 cities6 = {'Подольск - МейлРУ': 50, 'Подольск - Румянцево': 43, 'Подольск - Можайка': 45}
-num = 1500  # Нужное количество километров
+print('Введите километраж')
+num = 350  # Нужное количество километров
 count = 0  # подсчет километрожа
 city, km = random.choice(list(cities2.items()))
 copy1 = city
@@ -20,14 +22,14 @@ spis = []
 spis.append(city.split(' - ')[1])
 
 
-while count < 1000:
+while count < num - 50:
 	circle = random.randint(1, 3)
-	print(circle)
+
 	if circle == 1:
 		string = spis[-1]
 		a = copy1.split(' - ')[0]
 		b = copy1.split(' - ')[1]
-		print('{} - {}  {}'.format(b, a, km))
+		print('{} -+ {}  {}'.format(b, a, km))
 		count += km
 		copy1 = b + ' - ' + a
 		copy2 = km
@@ -62,17 +64,20 @@ while count < 1000:
 		else:
 			print('Error!, Try again!')
 
-		print('{}   {}'.format(city, km))
+		print('{}  ==  {}'.format(city, km))
 		copy1 = city
 		copy2 = km
+		count += km
 		spis.append(city.split(' - ')[-1])
 		for key, val in cities2.items():
 			if spis[-1] in key:
 				q = key.split(' - ')[0]
 				w = key.split(' - ')[1]
-		print('{} - {}   {}'.format(w, q, val))
+				po = val
+		print('{} --- {}   {}'.format(w, q, po))
 		copy1 = w +' - ' + q
 		copy2 = km
+		count += val
 		spis.append(q)
 
 	elif circle == 3:
@@ -104,9 +109,10 @@ while count < 1000:
 		else:
 			print('Error!, Try again!')
 
-		print('{}   {}'.format(city, km))
+		print('{}  !! {}'.format(city, km))
 		copy1 = city
 		copy2 = km
+		count += km
 		spis.append(city.split(' - ')[-1])
 
 		for key, val in al.items():
@@ -117,9 +123,10 @@ while count < 1000:
 				w = key.split(' - ')[1]
 
 
-		print('{} + {}   {}'.format(q, w, val))
+		print('{} -! {}   {}'.format(q, w, val))
 		copy1 = w + ' - ' + w
 		copy2 = km
+		count += val
 		spis.append(w)
 		for key, val in cities2.items():
 			if spis[-1] in key:    # здесь нужно не сразу использовать места, а сначал собрать вместе, а потом выбрать из них на рандом, чтоб избежать многократных повторений
@@ -127,7 +134,17 @@ while count < 1000:
 				w = key.split(' - ')[1]
 
 
-		print('{} -- {}   {}'.format(w, q, val))
+		print('{} -) {}   {}'.format(w, q, val))
 		copy1 = w +' - ' + q
 		copy2 = km
+		count += val
 		spis.append(q)
+
+if spis[-1] != 'Алтуфьево':
+	for key, val in al.items():
+		if 'Алтуфьево' in key and spis[-1] in key:
+			km = val
+	print('{} - Алтуфьево   {}'.format(spis[-1], km))
+	count += km
+print(count)
+
